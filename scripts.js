3c9ed743cac6f10b1cd18fedfa856eda4bab82e9
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-01-17 10:34:37 -0500

    create school object, add a teacher and a student.

--- scripts.js
+++ scripts.js
@@ -59,25 +59,55 @@ function Person(name, age, city) {
 	this.city = city;
 }
 
-function Teacher(title, subject) {
+function Teacher(name,age,city,title, subject) {
 	this.title = title;
 	this.subject = subject;
 	Person.apply(this, arguments);
 }
 
 Teacher.prototype = new Person();
+Teacher.prototype.constructor = Teacher;
 
-function Student(grade, major) {
+function Student(name,age,city,grade, major) {
 	this.grade = grade;
 	this.major = major;
 	Person.apply(this, arguments);
 }
 
 Student.prototype = new Person();
+Student.prototype.constructor = Student;
 
 fred = new Person("Fred",22,"Philly");
-george = new Teacher("George",32,"NYC");
-ron = new Student("Ron",15,"London");
-console.log(fred);
-console.log(george);
-console.log(ron);
+george = new Teacher("George",32,"NYC","Mr.","Defence Against the Dark Arts");
+ron = new Student("Ron",15,"London","5","Math");
+function School(name) {
+	this.name = name;
+	this.teachers = [];
+
+	this.listTeachers = function() {
+		var teacherList = "The teachers at" + this.name + " are: ";
+		for (var i = 0; i < this.teachers.length-1; i++) {
+			teacherList += this.teachers[i].name + ", ";
+		}
+		teacherList += this.teachers[this.teachers.length-1].name;
+		return teacherList;
+	}
+
+	this.students = [];
+
+	this.listStudents = function() {
+	var studentList = "The students at" + this.name + " are: ";
+	for (var i = 0; i < this.students.length-1; i++) {
+		studentList += this.students[i].name + ", ";
+	}
+	studentList += this.students[this.students.length-1].name;
+	return studentList;
+	}
+}
+
+sidewaysSchool = new School("Sideways School");
+sidewaysSchool.teachers.push(george);
+sidewaysSchool.students.push(ron);
+console.log(sidewaysSchool);
+console.log(sidewaysSchool.listTeachers());
+console.log(sidewaysSchool.listStudents());
